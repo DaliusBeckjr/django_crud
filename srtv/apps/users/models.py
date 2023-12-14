9296e906d886407a28122a646e177fda4e7c2391
Dalius Beck <41458059+DaliusBeckjr@users.noreply.github.com>
2023-12-14 13:00:14 -0700

    Update models.py

--- srtv/apps/users/models.py
+++ srtv/apps/users/models.py
@@ -11,23 +11,39 @@ class UserManager(models.Manager):
         
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         
-        if len(postData["first_name"]) < 0:
-            errors["first_name"] = "first Name field cannot be empty"
-        
-        
+        if len(postData["first_name"]) < 1:
+            errors["first_name"] = "first Name field is required"
+        elif len(postData["firs_name"]) < 3:
+            errors["first_name"] = "First Name field must be more than 3 characters"
+        # last name validation
+        if len(postData["last_name"]) < 1:
+            errors["last_name"] = "Last Name field is required"
+        elif len(postData["last_name"]) < 3: 
+            errors["last_name"] = "Last Name field must be more than 3 characters"
+        # email validaation
         if not EMAIL_REGEX.match(postData['email']):
             errors['email'] = "invalid email address"
+        # password validation
+        if len(postData["password"]) < 1:
+            errors['password'] = "Password is required"
+        elif len(postData["password"]) < 8:
+            errors["password"] = "Password needs to be more than 8 characters"
+        # confirm password 
+        # if len(postData["confirm_pw"]) < 1:
+        #     errors['confirm_pw'] = "Confimr password field is required"
         return errors
 
 
 
 class User(models.Model):
-    first_name = models.Charfield(max_length=45)
-    last_name = models.Charfield(max_length=45)
-    email = models.Emailfield(unique=True)
-    password = models.Charfield(max_length = 60)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length = 60)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
 
     def __str__(self):
         return f"""user object():
